d53e6023c009cca80a57a4e55657eac55eb0170b
Stanislau Turasau <pontty232@gmail.com>
2021-08-09 22:43:25 +0300

    feat: add function which show result table

--- hometask04/script.js
+++ hometask04/script.js
@@ -1,17 +1,22 @@
+// variable declaration
+const btnArr = document.getElementById("btn_array");
+const resultArray = document.getElementById("res_arr");
+const btnRes = document.getElementById("btn_res_tab");
+
 // generate random number by Gaussian distribution
-function makeGaussDistr() { 
+function makeGaussDistr() {
   let u = 0;
-  let v = 0; 
-  while (u === 0) u = Math.random(); 
-  while (v === 0) v = Math.random(); 
-  return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v) * 2 | 0;
-};
+  let v = 0;
+  while (u === 0) u = Math.random();
+  while (v === 0) v = Math.random();
+  return (Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v) * 2) | 0;
+}
 
 // generate array with random numbers
 function generateArrWithRandomsN() {
   const arr = [];
 
-  for(let i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) {
     arr.push(makeGaussDistr());
   }
 
@@ -20,10 +25,50 @@ function generateArrWithRandomsN() {
 
 // generate object with count each element of array
 function generateObjectByArray(arr) {
-  const result = {}; 
+  const result = {};
 
-  arr.map(x => (x in result) ? (result[x] += 1) : (result[x] = 1));
+  arr.map((x) => (x in result ? (result[x] += 1) : (result[x] = 1)));
 
   return result;
 }
 
+// show our array with Gaussian distribution in the input
+btnArr.addEventListener("click", () => {
+  resultArray.value = generateArrWithRandomsN();
+  resultArray.classList.remove("hidden");
+});
+
+// show resulting table with our key-values
+btnRes.addEventListener("click", () => {
+  const arr = resultArray.value.split(",");
+  const obj = generateObjectByArray(arr);
+  const length = Object.keys(obj).length;
+  const table = document.getElementById("res_tab");
+  const tbody = document.getElementsByTagName("tbody")[0];
+
+  if (tbody.hasChildNodes()) {
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+  }
+
+  table.classList.remove("hidden");
+
+  for (let i = 0; i < length; i++) {
+    const newRow = document.createElement("tr");
+    const newNumCol = document.createElement("td");
+    const newCountCol = document.createElement("td");
+
+    newNumCol.appendChild(document.createTextNode(`${Object.keys(obj)[i]}`));
+    newNumCol.style.textAlign = "center";
+    newCountCol.appendChild(
+      document.createTextNode(`${Object.values(obj)[i]}`)
+    );
+    newCountCol.style.textAlign = "center";
+
+    newRow.appendChild(newNumCol);
+    newRow.appendChild(newCountCol);
+
+    tbody.appendChild(newRow);
+  }
+});
