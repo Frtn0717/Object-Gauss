bd8ab2cc6619d8141a50eb64cbc6fc777798e49f
Stanislau Turasau <pontty232@gmail.com>
2021-08-18 19:11:01 +0300

    refactor: replace table with "show result" button

--- hometask04/script.js
+++ hometask04/script.js
@@ -1,7 +1,7 @@
 // variable declaration
-const btnArr = document.getElementById("btn_array");
-const resultArray = document.getElementById("res_arr");
-const btnRes = document.getElementById("btn_res_tab");
+const btnArr = document.getElementById('btn_array');
+const resultArray = document.getElementById('res_arr');
+const btnRes = document.getElementById('btn_res_tab');
 
 // generate random number by Gaussian distribution
 function makeGaussDistr() {
@@ -33,20 +33,22 @@ function generateObjectByArray(arr) {
 }
 
 // show our array with Gaussian distribution in the input
-btnArr.addEventListener("click", () => {
+btnArr.addEventListener('click', () => {
   resultArray.value = generateArrWithRandomsN();
-  resultArray.classList.remove("hidden");
-  btnRes.classList.remove("hidden");
+  resultArray.classList.remove('hidden');
+  btnRes.classList.remove('hidden');
 });
 
 // show resulting table with our key-values
-btnRes.addEventListener("click", () => {
-  const arr = resultArray.value.split(",");
+btnRes.addEventListener('click', () => {
+  const arr = resultArray.value.split(',');
   const obj = generateObjectByArray(arr);
-  const sortedKeys = Object.keys(generateObjectByArray(arr)).sort((prev, curr) => prev - curr);
+  const sortedKeys = Object.keys(generateObjectByArray(arr)).sort(
+    (prev, curr) => prev - curr
+  );
   const length = Object.keys(obj).length;
-  const table = document.getElementById("res_tab");
-  const tbody = document.getElementsByTagName("tbody")[0];
+  const table = document.getElementById('res_tab');
+  const tbody = document.getElementsByTagName('tbody')[0];
 
   if (tbody.hasChildNodes()) {
     while (tbody.firstChild) {
@@ -54,20 +56,18 @@ btnRes.addEventListener("click", () => {
     }
   }
 
-  table.classList.remove("hidden");
+  table.classList.remove('hidden');
 
   for (let i = 0; i < length; i++) {
-    const newRow = document.createElement("tr");
-    const newNumCol = document.createElement("td");
-    const newCountCol = document.createElement("td");
+    const newRow = document.createElement('tr');
+    const newNumCol = document.createElement('td');
+    const newCountCol = document.createElement('td');
     const value = obj[sortedKeys[i]];
 
     newNumCol.appendChild(document.createTextNode(`${sortedKeys[i]}`));
-    newNumCol.style.textAlign = "center";
-    newCountCol.appendChild(
-      document.createTextNode(`${value}`)
-    );
-    newCountCol.style.textAlign = "center";
+    newNumCol.style.textAlign = 'center';
+    newCountCol.appendChild(document.createTextNode(`${value}`));
+    newCountCol.style.textAlign = 'center';
 
     newRow.appendChild(newNumCol);
     newRow.appendChild(newCountCol);
